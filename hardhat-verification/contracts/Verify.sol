fa11bdf711ff52bef54b83d66d85956f73d8e17f
MSBivens <mikebiv95@gmail.com>
2022-07-07 22:01:14 -0500

    updated

--- hardhat-verification/contracts/Verify.sol
+++ hardhat-verification/contracts/Verify.sol
@@ -8,7 +8,7 @@ contract Verify {
 
     function hello(bool sayHello) public pure returns (string memory) {
         if (sayHello) {
-            return "hello";
+            return "hello world";
         }
         return "";
     }
